d4c7e996e1d7cf79c2e1af4fb206bb6972e0737f
Hugo Louro <hmclouro@gmail.com>
2015-08-27 08:35:12 -0700

    BUG-43366: Fix CacheBackedStorageManager get api 1. Return null value when attempting to load nonexistent key 2. Add unit tests

--- core/src/main/java/com/hortonworks/iotas/cache/impl/GuavaCache.java
+++ core/src/main/java/com/hortonworks/iotas/cache/impl/GuavaCache.java
@@ -4,7 +4,9 @@ import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.util.concurrent.UncheckedExecutionException;
 import com.hortonworks.iotas.cache.Cache;
+import com.hortonworks.iotas.cache.exception.NonexistentStorableKeyException;
 import com.hortonworks.iotas.cache.stats.CacheStats;
 import com.hortonworks.iotas.storage.Storable;
 import com.hortonworks.iotas.storage.StorableKey;
@@ -28,18 +30,33 @@ public class GuavaCache implements Cache<StorableKey, Storable> {
         this.dao = dao;
         this.guavaCache = guavaCacheBuilder.build(new CacheLoader<StorableKey, Storable>() {
             @Override
-            public Storable load(StorableKey key) throws StorageException {
-                return dao.get(key);
+            public Storable load(StorableKey key) throws StorageException, NonexistentStorableKeyException {
+                Storable val = dao.get(key);
+                if (val != null) {
+                    return val;
+                }
+                throw new NonexistentStorableKeyException("Nonexistent key : [" + key + "]");
             }
         });
     }
 
     public Storable get(StorableKey key) throws StorageException {
+        Storable val = null;
         try {
-            return guavaCache.get(key);
+            val = guavaCache.get(key);
+        } catch (UncheckedExecutionException e) {
+            // we need to handle the NonexistentStorableKeyException like this because
+            // the method com.google.common.cache.LocalCache.LoadingValueReference.loadFuture() (line #3544 as of guava 18)
+            // catches a throwable and converts it to another form of exception
+            if (e.getCause() instanceof NonexistentStorableKeyException) {
+                log.debug("Failed to load nonexistent key [" + key + "]. Returning null value." + Thread.currentThread());
+                return val;
+            }
+            throw new StorageException(e);
         } catch (ExecutionException e) {
             throw new StorageException(e);
         }
+        return val;
     }
 
     public ImmutableMap<StorableKey, Storable> getAllPresent(Iterable<? extends StorableKey> keys) {
