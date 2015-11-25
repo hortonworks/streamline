satishd/streams

IOT-47: Normalizer processor implementation.

layout/src/main/java/com/hortonworks/iotas/layout/runtime/n11n/TransformerRuntime.java
@@ -1,0 +1,108 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hortonworks.iotas.layout.runtime.n11n;
+
+import com.hortonworks.iotas.common.IotasEvent;
+import com.hortonworks.iotas.common.Schema;
+import com.hortonworks.iotas.layout.design.n11n.Transformer;
+import com.hortonworks.iotas.layout.runtime.script.GroovyScript;
+import com.hortonworks.iotas.layout.runtime.script.engine.GroovyScriptEngine;
+import com.hortonworks.iotas.parser.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.script.ScriptException;
+
+/**
+ *
+ */
+public class TransformerRuntime {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TransformerRuntime.class);
+
+    private final Transformer transformer;
+    private GroovyScript<? extends Object> groovyScript;
+
+    private TransformerRuntime(Transformer transformer, GroovyScript<? extends Object> groovyScript) {
+        this.transformer = transformer;
+        this.groovyScript = groovyScript;
+    }
+
+    public Object execute(IotasEvent iotasEvent) throws NormalizationException {
+        try {
+            String inputFieldName = transformer.getInputField().getName();
+            if(!iotasEvent.getFieldsAndValues().containsKey(inputFieldName)) {
+                throw new IllegalArgumentException("IotasEvent does not have input field: "+inputFieldName);
+            }
+
+            Object value = null;
+            if(groovyScript != null) {
+                LOGGER.debug("Running script [{}] with input [{}]", groovyScript, iotasEvent);
+
+                value = groovyScript.evaluate(iotasEvent);
+
+                LOGGER.debug("Computed value is {}. transformer: [{}] script: [{}] input: [{}]", value, transformer, groovyScript, iotasEvent);
+                Schema.Type type = transformer.getOutputField().getType();
+                if (!type.equals(Schema.fromJavaType(value))) {
+                    throw new NormalizationException("Computed value is not of expected type: " + type);
+                }
+            } else {
+                value = iotasEvent.getFieldsAndValues().get(inputFieldName);
+                LOGGER.debug("Input field value returned: {}", value);
+            }
+
+            return value;
+        } catch (ScriptException | ParseException e) {
+            LOGGER.error("Error occurred while converting input fields in tranformer: "+transformer);
+            throw new NormalizationException("Error occurred while converting input fields in a normalization", e);
+        }
+
+    }
+
+    public Transformer getTransformer() {
+        return transformer;
+    }
+
+    public static class Builder {
+        private Transformer transformer;
+
+        public Builder(Transformer transformer) {
+            this.transformer = transformer;
+        }
+
+        public TransformerRuntime build() throws NormalizationException {
+            if(transformer.getInputField() == null) {
+                throw new NormalizationException("input field should always be set for transformer");
+            }
+            //todo may add more validation for script.
+            GroovyScript groovyScript = null;
+            if(transformer.getConverterScript() != null) {
+                groovyScript = new GroovyScript<>(transformer.getConverterScript(), new GroovyScriptEngine());
+            }
+
+            return new TransformerRuntime(transformer, groovyScript);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "TransformerRuntime{" +
+                "transformer=" + transformer +
+                ", groovyScript=" + groovyScript +
+                '}';
+    }
+}

layout/src/main/java/com/hortonworks/iotas/layout/runtime/n11n/ValueGeneratorRuntime.java
@@ -1,0 +1,128 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hortonworks.iotas.layout.runtime.n11n;
+
+import com.hortonworks.iotas.common.IotasEvent;
+import com.hortonworks.iotas.common.Schema;
+import com.hortonworks.iotas.layout.design.n11n.ValueGenerator;
+import com.hortonworks.iotas.layout.runtime.script.GroovyScript;
+import com.hortonworks.iotas.layout.runtime.script.engine.GroovyScriptEngine;
+import com.hortonworks.iotas.parser.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.script.ScriptException;
+
+/**
+ *
+ */
+public class ValueGeneratorRuntime {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ValueGeneratorRuntime.class);
+
+    private final Schema.Field field;
+    private GroovyScript<Object> groovyScript;
+    private Object value;
+
+    public ValueGeneratorRuntime(Schema.Field field) {
+        this.field = field;
+    }
+
+    public Object generateValue(IotasEvent iotasEvent) throws NormalizationException {
+        if(value != null) {
+            LOGGER.debug("Returning default static value [{}] for [{}]", value, iotasEvent);
+            return value;
+        }
+
+        try {
+            LOGGER.debug("Running script [{}] with input [{}]", groovyScript, iotasEvent);
+            Object evaluatedValue = groovyScript.evaluate(iotasEvent);
+
+            LOGGER.debug("Computed value is {}. field: [{}] script: [{}] input: [{}]", value, field, groovyScript, iotasEvent);
+            Schema.Type type = field.getType();
+            if(!type.valueOfSameType(evaluatedValue)) {
+                throw new NormalizationException("Computed value is not of expected type: "+ type);
+            }
+            return evaluatedValue;
+        } catch (ScriptException | ParseException e) {
+            throw new NormalizationException(e);
+        }
+    }
+
+
+    public Schema.Field getField() {
+        return field;
+    }
+
+    public static class Builder {
+        private Schema.Field field;
+        private String script;
+        private Object value;
+
+        public Builder withValueGenerator(ValueGenerator valueGenerator) {
+            this.field = valueGenerator.getField();
+            this.script = valueGenerator.getScript();
+            this.value = valueGenerator.getValue();
+
+            return this;
+        }
+
+        public Builder withField(Schema.Field field) {
+            this.field = field;
+            return this;
+        }
+
+        public Builder withScript(String script) {
+            this.script = script;
+            return this;
+        }
+
+        public Builder withValue(Object value) {
+            this.value = value;
+            return this;
+        }
+
+        public ValueGeneratorRuntime build() throws NormalizationException {
+
+            if(field == null) {
+                throw new NormalizationException("field should always be set.");
+            }
+
+            if(value == null && script == null) {
+                throw new NormalizationException("Either value or script must exist.");
+            }
+
+            ValueGeneratorRuntime valueGeneratorRuntime = new ValueGeneratorRuntime(field);
+            if(value != null) {
+                valueGeneratorRuntime.value = value;
+            } else if(script != null) {
+                valueGeneratorRuntime.groovyScript = new GroovyScript<>(script, new GroovyScriptEngine());
+            }
+
+            return valueGeneratorRuntime;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "ValueGeneratorRuntime{" +
+                "field=" + field +
+                ", groovyScript=" + groovyScript +
+                ", value=" + value +
+                '}';
+    }
+}

storm/src/main/java/com/hortonworks/bolt/n11n/NormalizationBolt.java
@@ -1,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hortonworks.bolt.n11n;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+import com.hortonworks.iotas.common.IotasEvent;
+import com.hortonworks.iotas.common.IotasEventImpl;
+import com.hortonworks.iotas.layout.runtime.n11n.NormalizationProcessorRuntime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *
+ */
+public class NormalizationBolt extends BaseRichBolt {
+    private static final Logger LOGGER = LoggerFactory.getLogger(NormalizationBolt.class);
+
+    private TopologyContext context;
+    private OutputCollector collector;
+    private NormalizationProcessorRuntime normalizationProcessorRuntime;
+
+    public NormalizationBolt(NormalizationProcessorRuntime normalizationProcessorRuntime) {
+        this.normalizationProcessorRuntime = normalizationProcessorRuntime;
+    }
+
+    @Override
+    public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
+        this.context = context;
+        this.collector = collector;
+    }
+
+    @Override
+    public void execute(Tuple input) {
+        IotasEvent iotasEvent = (IotasEvent) input.getValueByField(IotasEvent.IOTAS_EVENT);
+        if(iotasEvent == null) {
+            throw new IllegalArgumentException("input tuple should contain a field with name '"+IotasEvent.IOTAS_EVENT+"'");
+        }
+
+        try {
+            Map<String, Object> resultMap = normalizationProcessorRuntime.execute(iotasEvent);
+            IotasEventImpl newIotasEvent = new IotasEventImpl(resultMap, iotasEvent.getDataSourceId(), iotasEvent.getId());
+            collector.emit(input.getSourceStreamId(), input, new Values(newIotasEvent));
+
+            collector.ack(input);
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while normalizing the tuple", e);
+            collector.fail(input);
+            collector.reportError(e);
+        }
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        if(context == null) {
+            throw new IllegalStateException("TopologyContext is null, prepare may not have been called.");
+        }
+
+        Set<String> thisStreams = context.getThisStreams();
+        for (String streamId : thisStreams) {
+            declarer.declareStream(streamId, new Fields(IotasEvent.IOTAS_EVENT));
+        }
+    }
+}

hortonworks/streamline

layout/src/main/java/com/hortonworks/iotas/layout/runtime/n11n/TransformerRuntime.java
@@ -1,0 +1,108 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hortonworks.iotas.layout.runtime.n11n;
+
+import com.hortonworks.iotas.common.IotasEvent;
+import com.hortonworks.iotas.common.Schema;
+import com.hortonworks.iotas.layout.design.n11n.Transformer;
+import com.hortonworks.iotas.layout.runtime.script.GroovyScript;
+import com.hortonworks.iotas.layout.runtime.script.engine.GroovyScriptEngine;
+import com.hortonworks.iotas.parser.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.script.ScriptException;
+
+/**
+ *
+ */
+public class TransformerRuntime {
+    private static final Logger LOG = LoggerFactory.getLogger(TransformerRuntime.class);
+
+    private final Transformer transformer;
+    private GroovyScript<? extends Object> groovyScript;
+
+    private TransformerRuntime(Transformer transformer, GroovyScript<? extends Object> groovyScript) {
+        this.transformer = transformer;
+        this.groovyScript = groovyScript;
+    }
+
+    public Object execute(IotasEvent iotasEvent) throws NormalizationException {
+        try {
+            String inputFieldName = transformer.getInputField().getName();
+            if(!iotasEvent.getFieldsAndValues().containsKey(inputFieldName)) {
+                throw new IllegalArgumentException("IotasEvent does not have input field: "+inputFieldName);
+            }
+
+            Object value = null;
+            if(groovyScript != null) {
+                LOG.debug("Running script [{}] with input [{}]", groovyScript, iotasEvent);
+
+                value = groovyScript.evaluate(iotasEvent);
+
+                LOG.debug("Computed value is {}. transformer: [{}] script: [{}] input: [{}]", value, transformer, groovyScript, iotasEvent);
+                Schema.Type type = transformer.getOutputField().getType();
+                if (!type.equals(Schema.fromJavaType(value))) {
+                    throw new NormalizationException("Computed value is not of expected type: " + type);
+                }
+            } else {
+                value = iotasEvent.getFieldsAndValues().get(inputFieldName);
+                LOG.debug("Input field value returned: {}", value);
+            }
+
+            return value;
+        } catch (ScriptException | ParseException e) {
+            LOG.error("Error occurred while converting input fields in tranformer: " + transformer);
+            throw new NormalizationException("Error occurred while converting input fields in a normalization", e);
+        }
+
+    }
+
+    public Transformer getTransformer() {
+        return transformer;
+    }
+
+    public static class Builder {
+        private Transformer transformer;
+
+        public Builder(Transformer transformer) {
+            this.transformer = transformer;
+        }
+
+        public TransformerRuntime build() throws NormalizationException {
+            if(transformer.getInputField() == null) {
+                throw new NormalizationException("input field should always be set for transformer");
+            }
+            //todo may add more validation for script.
+            GroovyScript groovyScript = null;
+            if(transformer.getConverterScript() != null) {
+                groovyScript = new GroovyScript<>(transformer.getConverterScript(), new GroovyScriptEngine());
+            }
+
+            return new TransformerRuntime(transformer, groovyScript);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "TransformerRuntime{" +
+                "transformer=" + transformer +
+                ", groovyScript=" + groovyScript +
+                '}';
+    }
+}

layout/src/main/java/com/hortonworks/iotas/layout/runtime/n11n/ValueGeneratorRuntime.java
@@ -1,0 +1,128 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hortonworks.iotas.layout.runtime.n11n;
+
+import com.hortonworks.iotas.common.IotasEvent;
+import com.hortonworks.iotas.common.Schema;
+import com.hortonworks.iotas.layout.design.n11n.ValueGenerator;
+import com.hortonworks.iotas.layout.runtime.script.GroovyScript;
+import com.hortonworks.iotas.layout.runtime.script.engine.GroovyScriptEngine;
+import com.hortonworks.iotas.parser.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.script.ScriptException;
+
+/**
+ *
+ */
+public class ValueGeneratorRuntime {
+    private static final Logger LOG = LoggerFactory.getLogger(ValueGeneratorRuntime.class);
+
+    private final Schema.Field field;
+    private GroovyScript<Object> groovyScript;
+    private Object value;
+
+    public ValueGeneratorRuntime(Schema.Field field) {
+        this.field = field;
+    }
+
+    public Object generateValue(IotasEvent iotasEvent) throws NormalizationException {
+        if(value != null) {
+            LOG.debug("Returning default static value [{}] for [{}]", value, iotasEvent);
+            return value;
+        }
+
+        try {
+            LOG.debug("Running script [{}] with input [{}]", groovyScript, iotasEvent);
+            Object evaluatedValue = groovyScript.evaluate(iotasEvent);
+
+            LOG.debug("Computed value is {}. field: [{}] script: [{}] input: [{}]", value, field, groovyScript, iotasEvent);
+            Schema.Type type = field.getType();
+            if(!type.valueOfSameType(evaluatedValue)) {
+                throw new NormalizationException("Computed value is not of expected type: "+ type);
+            }
+            return evaluatedValue;
+        } catch (ScriptException | ParseException e) {
+            throw new NormalizationException(e);
+        }
+    }
+
+
+    public Schema.Field getField() {
+        return field;
+    }
+
+    public static class Builder {
+        private Schema.Field field;
+        private String script;
+        private Object value;
+
+        public Builder withValueGenerator(ValueGenerator valueGenerator) {
+            this.field = valueGenerator.getField();
+            this.script = valueGenerator.getScript();
+            this.value = valueGenerator.getValue();
+
+            return this;
+        }
+
+        public Builder withField(Schema.Field field) {
+            this.field = field;
+            return this;
+        }
+
+        public Builder withScript(String script) {
+            this.script = script;
+            return this;
+        }
+
+        public Builder withValue(Object value) {
+            this.value = value;
+            return this;
+        }
+
+        public ValueGeneratorRuntime build() throws NormalizationException {
+
+            if(field == null) {
+                throw new NormalizationException("field should always be set.");
+            }
+
+            if(value == null && script == null) {
+                throw new NormalizationException("Either value or script must exist.");
+            }
+
+            ValueGeneratorRuntime valueGeneratorRuntime = new ValueGeneratorRuntime(field);
+            if(value != null) {
+                valueGeneratorRuntime.value = value;
+            } else if(script != null) {
+                valueGeneratorRuntime.groovyScript = new GroovyScript<>(script, new GroovyScriptEngine());
+            }
+
+            return valueGeneratorRuntime;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "ValueGeneratorRuntime{" +
+                "field=" + field +
+                ", groovyScript=" + groovyScript +
+                ", value=" + value +
+                '}';
+    }
+}

storm/src/main/java/com/hortonworks/bolt/n11n/NormalizationBolt.java
@@ -1,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hortonworks.bolt.n11n;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+import com.hortonworks.iotas.common.IotasEvent;
+import com.hortonworks.iotas.common.IotasEventImpl;
+import com.hortonworks.iotas.layout.runtime.n11n.NormalizationProcessorRuntime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *
+ */
+public class NormalizationBolt extends BaseRichBolt {
+    private static final Logger LOG = LoggerFactory.getLogger(NormalizationBolt.class);
+
+    private TopologyContext context;
+    private OutputCollector collector;
+    private NormalizationProcessorRuntime normalizationProcessorRuntime;
+
+    public NormalizationBolt(NormalizationProcessorRuntime normalizationProcessorRuntime) {
+        this.normalizationProcessorRuntime = normalizationProcessorRuntime;
+    }
+
+    @Override
+    public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
+        this.context = context;
+        this.collector = collector;
+    }
+
+    @Override
+    public void execute(Tuple input) {
+        IotasEvent iotasEvent = (IotasEvent) input.getValueByField(IotasEvent.IOTAS_EVENT);
+        if(iotasEvent == null) {
+            throw new IllegalArgumentException("input tuple should contain a field with name '"+IotasEvent.IOTAS_EVENT+"'");
+        }
+
+        try {
+            Map<String, Object> resultMap = normalizationProcessorRuntime.execute(iotasEvent);
+            IotasEventImpl newIotasEvent = new IotasEventImpl(resultMap, iotasEvent.getDataSourceId(), iotasEvent.getId());
+            collector.emit(input.getSourceStreamId(), input, new Values(newIotasEvent));
+
+            collector.ack(input);
+        } catch (Exception e) {
+            LOG.error("Error occurred while normalizing the tuple", e);
+            collector.fail(input);
+            collector.reportError(e);
+        }
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        if(context == null) {
+            throw new IllegalStateException("TopologyContext is null, prepare may not have been called.");
+        }
+
+        Set<String> thisStreams = context.getThisStreams();
+        for (String streamId : thisStreams) {
+            declarer.declareStream(streamId, new Fields(IotasEvent.IOTAS_EVENT));
+        }
+    }
+}