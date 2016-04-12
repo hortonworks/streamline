6f0b7ad27728ee953663ee4d14897882baef01c6
Satish Duggana <sduggana@hortonworks.com>
2016-04-12 09:02:22 +0530

    Addressed review comments regarding wildcard imports

--- core/src/main/java/com/hortonworks/iotas/storage/impl/jdbc/JdbcStorageManager.java
+++ core/src/main/java/com/hortonworks/iotas/storage/impl/jdbc/JdbcStorageManager.java
@@ -36,7 +36,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
-import java.util.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 //TODO: Need to assess synchronization
 public class JdbcStorageManager implements StorageManager {
