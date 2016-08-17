8994778aec1d2beb686e36255b5e63f0f917047e
Priyank <pshah@hortonworks.com>
2016-08-17 13:42:15 -0700

    IOT-347: Addressing review comments

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash 
 
 verbose=false
+bootstrap_dir=$(dirname $0)
 
 function run_cmd {
   cmd=$*
@@ -40,40 +41,41 @@ function post {
 
 function usage {
   cat <<-EOF 
-$0 [-vh]
-   -v verbose
-   -h help
+$0 [-vh] [-d bootstrap_dir]
+   -v               verbose
+   -h               help
+   -d bootstrap_dir specify the bootstrap directory
 EOF
   exit 0
 }
 
-while getopts 'hv' flag; do
+while getopts 'hvd:' flag; do
   case "${flag}" in
     h) usage ;;
     v) verbose='true' ;;
+    d) bootstrap_dir=$OPTARG ;;
     *) error "Unexpected option ${flag}" ;;
   esac
 done
 
-base_dir=$(dirname $0)
-post /system/componentdefinitions/SOURCE $base_dir/kafka-topology-component
-post /system/componentdefinitions/PROCESSOR $base_dir/rule-topology-component
-post /system/componentdefinitions/PROCESSOR $base_dir/parser-topology-component
-post /system/componentdefinitions/PROCESSOR $base_dir/normalization-processor-topology-component
-post /system/componentdefinitions/SINK $base_dir/hdfs-topology-component
-post /system/componentdefinitions/SINK $base_dir/hbase-topology-component
-post /system/componentdefinitions/SINK $base_dir/notification-topology-component
-post /system/componentdefinitions/SINK $base_dir/opentsdb-sink-topology-component
-post /system/componentdefinitions/LINK $base_dir/all-grouping-link-topology-component
-post /system/componentdefinitions/LINK $base_dir/direct-grouping-link-topology-component
-post /system/componentdefinitions/LINK $base_dir/shuffle-grouping-link-topology-component
-post /system/componentdefinitions/LINK $base_dir/local-or-shuffle-grouping-link-topology-component
-post /system/componentdefinitions/LINK $base_dir/fields-grouping-link-topology-component
-post /system/componentdefinitions/LINK $base_dir/global-grouping-link-topology-component
-post /system/componentdefinitions/LINK $base_dir/none-grouping-link-topology-component
-post /system/componentdefinitions/PROCESSOR $base_dir/split-topology-component
-post /system/componentdefinitions/PROCESSOR $base_dir/join-topology-component
-post /system/componentdefinitions/PROCESSOR $base_dir/stage-topology-component
-post /system/componentdefinitions/ACTION $base_dir/transform-action-topology-component
-post /system/componentdefinitions/TRANSFORM $base_dir/projection-transform-topology-component
-post /system/componentdefinitions/TRANSFORM $base_dir/enrichment-transform-topology-component
+post /system/componentdefinitions/SOURCE $bootstrap_dir/kafka-topology-component
+post /system/componentdefinitions/PROCESSOR $bootstrap_dir/rule-topology-component
+post /system/componentdefinitions/PROCESSOR $bootstrap_dir/parser-topology-component
+post /system/componentdefinitions/PROCESSOR $bootstrap_dir/normalization-processor-topology-component
+post /system/componentdefinitions/SINK $bootstrap_dir/hdfs-topology-component
+post /system/componentdefinitions/SINK $bootstrap_dir/hbase-topology-component
+post /system/componentdefinitions/SINK $bootstrap_dir/notification-topology-component
+post /system/componentdefinitions/SINK $bootstrap_dir/opentsdb-sink-topology-component
+post /system/componentdefinitions/LINK $bootstrap_dir/all-grouping-link-topology-component
+post /system/componentdefinitions/LINK $bootstrap_dir/direct-grouping-link-topology-component
+post /system/componentdefinitions/LINK $bootstrap_dir/shuffle-grouping-link-topology-component
+post /system/componentdefinitions/LINK $bootstrap_dir/local-or-shuffle-grouping-link-topology-component
+post /system/componentdefinitions/LINK $bootstrap_dir/fields-grouping-link-topology-component
+post /system/componentdefinitions/LINK $bootstrap_dir/global-grouping-link-topology-component
+post /system/componentdefinitions/LINK $bootstrap_dir/none-grouping-link-topology-component
+post /system/componentdefinitions/PROCESSOR $bootstrap_dir/split-topology-component
+post /system/componentdefinitions/PROCESSOR $bootstrap_dir/join-topology-component
+post /system/componentdefinitions/PROCESSOR $bootstrap_dir/stage-topology-component
+post /system/componentdefinitions/ACTION $bootstrap_dir/transform-action-topology-component
+post /system/componentdefinitions/TRANSFORM $bootstrap_dir/projection-transform-topology-component
+post /system/componentdefinitions/TRANSFORM $bootstrap_dir/enrichment-transform-topology-component
