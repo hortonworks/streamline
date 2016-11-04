1cf6ab1b0c8912e8c2db81575afd83bc757cba0c
Hugo Louro <hmclouro@gmail.com>
2016-11-03 17:14:35 -0700

    Remove no longer needed setup files
    Change-Id: I4920d64cf1ed0c153187591395126acf20273b9b

--- bin/load-json-device.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-curl -X POST -i -F parserJar=@../parsers/target/parsers-0.1.0-SNAPSHOT.jar -F parserInfo='{"id":1, "name":"SimpleJsonParser", "parserSchema":{"fields":[{"name":"field1", "type":"STRING"},{"name":"humidity", "type":"INTEGER"}]}, "className":"org.apache.streamline.parsers.json.JsonParser","version":1}' http://localhost:8080/api/v1/catalog/parsers &&\
-curl -X POST http://localhost:8080/api/v1/catalog/datasources --data @simple-json-datasource -H "Content-Type: application/json"
-curl -X POST http://localhost:8080/api/v1/catalog/datasources --data @nested-json-datasource -H "Content-Type: application/json"
