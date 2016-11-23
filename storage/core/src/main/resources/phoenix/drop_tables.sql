8655b771ea791161a6485ebab3ca2d5194313f42
Sriharsha Chintalapani <harsha@hortonworks.com>
2016-11-22 19:46:48 -0800

    Added missing drop table statement from topology_editor_metadata.

--- storage/core/src/main/resources/phoenix/drop_tables.sql
+++ storage/core/src/main/resources/phoenix/drop_tables.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS parser_info
 DROP TABLE IF EXISTS topology_versioninfos
 DROP TABLE IF EXISTS topologies
+DROP TABLE IF EXISTS topology_editor_metadata
 DROP TABLE IF EXISTS topology_component_bundles
 DROP TABLE IF EXISTS files
 DROP TABLE IF EXISTS tag
