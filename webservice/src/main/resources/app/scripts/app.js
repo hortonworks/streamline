376938fe1ee150e0b783d9b16b07260b3db20c09
Sanket Shah <er.sanketshah@gmail.com>
2016-12-13 23:27:25 +0530

    Changes for calling dashboard API into pivot ui
    Signed-off-by: Sriharsha Chintalapani <harsha@hortonworks.com>

--- webservice/src/main/resources/app/scripts/app.js
+++ webservice/src/main/resources/app/scripts/app.js
@@ -2,11 +2,30 @@ import React, { Component, PropTypes } from 'react'
 import routes from './routers/routes'
 import { render } from 'react-dom';
 import { Router, browserHistory, hashHistory } from 'react-router';
+import fetch from 'isomorphic-fetch';
+import {baseUrl} from './utils/Constants';
 
 class App extends Component {
 	constructor(){
 		super()
+		this.validateDashboardAPI()
 	}
+	validateDashboardAPI(){
+		let apiURL = '/api/v1/dashboards';
+		fetch(apiURL, {method: 'GET'}).then((res)=>{return res.json()}).then((result)=>{
+			if(result.entities.length == 0){
+				fetch(apiURL, {
+					method: 'POST',
+					body: JSON.stringify({name: 'dashboard', description: 'dashboard', data: ''}),
+					headers: {
+						'Content-Type' : 'application/json',
+						'Accept' : 'application/json'
+					}
+				})
+			}
+		})
+	}
+
   render() {
     return (
       <Router ref="router" history={hashHistory} routes={routes} />
