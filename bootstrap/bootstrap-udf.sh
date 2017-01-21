f3447ebfb90c5910a4e5275e66a177ab195f71d8
Roshan Naik <roshan@hortonworks.com>
2017-01-20 22:05:35 -0800

    Fixes to bootstrap-notifiers.sh & bootstrap-notifiers.sh to find the jars correctly. Plus minor clean up of console output.

--- bootstrap/bootstrap-udf.sh
+++ bootstrap/bootstrap-udf.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 dir=$(dirname $0)/..
+bootstrap_dir=$(dirname $0)
 CATALOG_ROOT_URL="${1:-http://localhost:8080/api/v1/catalog}"
 
-jarFile=./udf-jars/streamline-functions-0.1.0-SNAPSHOT.jar
+jarFile=${bootstrap_dir}/udf-jars/streamline-functions-0.1.0-SNAPSHOT.jar
 if [[ ! -f ${jarFile} ]]
 then
   # try local build path
@@ -17,60 +18,41 @@ fi
 
 # Load UDF functions
 echo "Adding aggregate functions"
-echo "stddev"
+echo "  - stddev"
 curl -s -X POST "${CATALOG_ROOT_URL}/streams/udfs" -F udfJarFile=@${jarFile} -F udfConfig='{"name":"STDDEV", "displayName": "STDDEV", "description": "Standard deviation", "type":"AGGREGATE", "className":"com.hortonworks.streamline.streams.udaf.Stddev"};type=application/json'
-echo
 
-echo "stddevp"
+echo "  - stddevp"
 curl -s -X POST "${CATALOG_ROOT_URL}/streams/udfs" -F udfJarFile=@${jarFile} -F udfConfig='{"name":"STDDEVP", "displayName": "STDDEVP", "description": "Population standard deviation", "type":"AGGREGATE", "className":"com.hortonworks.streamline.streams.udaf.Stddevp"};type=application/json'
-echo
 
-echo "variance"
+echo "  - variance"
 curl -s -X POST "${CATALOG_ROOT_URL}/streams/udfs" -F udfJarFile=@${jarFile} -F udfConfig='{"name":"VARIANCE", "displayName": "VARIANCE", "description": "Variance", "type":"AGGREGATE", "className":"com.hortonworks.streamline.streams.udaf.Variance"};type=application/json'
-echo
 
-echo "variancep"
+echo "  - variancep"
 curl -s -X POST "${CATALOG_ROOT_URL}/streams/udfs" -F udfJarFile=@${jarFile} -F udfConfig='{"name":"VARIANCEP", "displayName": "VARIANCEP", "description": "Population variance", "type":"AGGREGATE", "className":"com.hortonworks.streamline.streams.udaf.Variancep"};type=application/json'
-echo
 
-echo "avg"
+echo "  - avg"
 curl -s -X POST "${CATALOG_ROOT_URL}/streams/udfs" -F udfJarFile=@${jarFile} -F udfConfig='{"name":"MEAN", "displayName": "AVG","description": "Average", "type":"AGGREGATE", "className":"com.hortonworks.streamline.streams.udaf.Mean"};type=application/json'
-echo
 
 # TODO: Code generation issues in calcite code generator. See https://github.com/hortonworks/streamline/pull/422#issuecomment-270330293
-#echo "collectlist"
+#echo "  - collectlist"
 #curl -s -X POST "${CATALOG_ROOT_URL}/streams/udfs" -F udfJarFile=@${jarFile} -F udfConfig='{"name":"COLLECTLIST", "displayName": "COLLECTLIST", "description": "Collect", "type":"AGGREGATE", "className":"com.hortonworks.streamline.streams.udaf.CollectList"};type=application/json'
-#echo
 
 # TODO: Code generation issues in calcite code generator. See https://github.com/hortonworks/streamline/pull/422#issuecomment-270330293
-#echo "topn"
+#echo "  - topn"
 #curl -s -X POST "${CATALOG_ROOT_URL}/streams/udfs" -F udfJarFile=@${jarFile} -F udfConfig='{"name":"TOPN", "displayName": "TOPN", "description": "Top N", "type":"AGGREGATE", "className":"com.hortonworks.streamline.streams.udaf.Topn"};type=application/json'
-#echo
 
-echo "identity"
+echo "  - identity"
 curl -s -X POST "${CATALOG_ROOT_URL}/streams/udfs" -F udfJarFile=@${jarFile} -F udfConfig='{"name":"IDENTITY", "displayName": "Identity", "description": "Identity function", "type":"FUNCTION", "className":"com.hortonworks.streamline.streams.udf.Identity"};type=application/json'
-echo
 
 # Dummy entries for built in functions so that it shows up in the UI
-echo "Adding dummy entries for builtin functions"
+echo "Adding builtin functions"
 curl -s -X POST "${CATALOG_ROOT_URL}/streams/udfs" -F udfConfig='{"name":"MIN", "displayName": "MIN", "description": "Minimum", "type":"AGGREGATE", "argTypes":["BOOLEAN|BYTE|SHORT|INTEGER|LONG|FLOAT|DOUBLE|STRING"], "className":"builtin"};type=application/json' -F builtin=true
-echo
 curl -s -X POST "${CATALOG_ROOT_URL}/streams/udfs" -F udfConfig='{"name":"MAX", "displayName": "MAX", "description": "Maximum", "type":"AGGREGATE", "argTypes":["BOOLEAN|BYTE|SHORT|INTEGER|LONG|FLOAT|DOUBLE|STRING"], "className":"builtin"};type=application/json' -F builtin=true
-echo
 curl -s -X POST "${CATALOG_ROOT_URL}/streams/udfs" -F udfConfig='{"name":"SUM", "displayName": "SUM", "description": "Sum", "type":"AGGREGATE", "argTypes":["BYTE|SHORT|INTEGER|LONG|FLOAT|DOUBLE"], "className":"builtin"};type=application/json' -F builtin=true
-echo
 curl -s -X POST "${CATALOG_ROOT_URL}/streams/udfs" -F udfConfig='{"name":"COUNT", "displayName": "COUNT", "description": "Count", "type":"AGGREGATE", "argTypes":["BOOLEAN|BYTE|SHORT|INTEGER|LONG|FLOAT|DOUBLE|STRING|BINARY|NESTED|ARRAY"], "returnType": "LONG", "className":"builtin"};type=application/json' -F builtin=true
-echo
 curl -s -X POST "${CATALOG_ROOT_URL}/streams/udfs" -F udfConfig='{"name":"UPPER", "displayName": "UPPER", "description": "Uppercase", "type":"FUNCTION", "argTypes":["STRING"], "returnType": "STRING", "className":"builtin"};type=application/json' -F builtin=true
-echo
 curl -s -X POST "${CATALOG_ROOT_URL}/streams/udfs" -F udfConfig='{"name":"LOWER", "displayName": "LOWER", "description": "Lowercase", "type":"FUNCTION", "argTypes":["STRING"], "returnType": "STRING", "className":"builtin"};type=application/json' -F builtin=true
-echo
 curl -s -X POST "${CATALOG_ROOT_URL}/streams/udfs" -F udfConfig='{"name":"INITCAP", "displayName": "INITCAP", "description": "First letter of each word in uppercase", "type":"FUNCTION", "argTypes":["STRING"], "returnType": "STRING", "className":"builtin"};type=application/json' -F builtin=true
-echo
 curl -s -X POST "${CATALOG_ROOT_URL}/streams/udfs" -F udfConfig='{"name":"SUBSTRING", "displayName": "SUBSTRING", "description": "Substring", "type":"FUNCTION", "argTypes":["STRING"], "returnType": "STRING", "className":"builtin"};type=application/json' -F builtin=true
-echo
 curl -s -X POST "${CATALOG_ROOT_URL}/streams/udfs" -F udfConfig='{"name":"CHAR_LENGTH", "displayName": "CHAR_LENGTH", "description": "String length", "type":"FUNCTION", "argTypes":["STRING"], "returnType": "LONG", "className":"builtin"};type=application/json' -F builtin=true
-echo
 curl -s -X POST "${CATALOG_ROOT_URL}/streams/udfs" -F udfConfig='{"name":"CONCAT", "displayName": "CONCAT", "description": "Concatenate", "type":"FUNCTION", "argTypes":["STRING"], "returnType": "STRING", "className":"builtin"};type=application/json' -F builtin=true
-echo
-echo "Done"
