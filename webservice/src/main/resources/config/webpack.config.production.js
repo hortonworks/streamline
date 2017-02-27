03df427dd68c4f86606e462d32a8683240eabd20
Sanket Shah <er.sanketshah@gmail.com>
2017-02-27 10:30:38 +0530

    Added license check for ui files in build process
    Signed-off-by: Sriharsha Chintalapani <harsha@hortonworks.com>

--- webservice/src/main/resources/config/webpack.config.production.js
+++ webservice/src/main/resources/config/webpack.config.production.js
@@ -1,16 +1,16 @@
 /**
- * Copyright 2017 Hortonworks.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *   http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- **/
+  * Copyright 2017 Hortonworks.
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *   http://www.apache.org/licenses/LICENSE-2.0
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+**/
 
 const path = require('path');
 const merge = require('webpack-merge');
@@ -75,19 +75,18 @@ module.exports = merge(config, {
         loader: ExtractTextPlugin.extract({
           fallbackLoader: 'style',
           loader: [{
-              loader: 'css',
-              query: {
-                sourceMap: true
-              }
-            },
-            'postcss',
-            {
-              loader: 'sass',
-              query: {
-                outputStyle: 'compressed'
-              }
+            loader: 'css',
+            query: {
+              sourceMap: true
             }
-          ]
+          },
+            'postcss',
+          {
+            loader: 'sass',
+            query: {
+              outputStyle: 'compressed'
+            }
+          }]
         })
       },
       // Sass + CSS Modules
@@ -120,5 +119,5 @@ module.exports = merge(config, {
         })
       }
     ]
-  },
+  }
 });
