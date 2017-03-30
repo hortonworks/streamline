48d77120782d876039f1b4784ab3e52e407376a7
Sanket Shah <er.sanketshah@gmail.com>
2017-03-30 10:09:46 +0530

    Fixed STREAMLINE-712, 698 and showing only conditions part in rule processor
    Signed-off-by: Sriharsha Chintalapani <harsha@hortonworks.com>

--- webservice/src/main/resources/app/scripts/containers/Streams/TopologyEditor/ProjectionProcessorContainer.jsx
+++ webservice/src/main/resources/app/scripts/containers/Streams/TopologyEditor/ProjectionProcessorContainer.jsx
@@ -177,12 +177,18 @@ export default class ProjectionProcessorContainer extends Component {
         outputStreams: []
       };
       TopologyREST.createNode(topologyId, versionId, "rules", {body: JSON.stringify(dummyProjectionObj)}).then((rulesNode) => {
-        this.projectionRulesNode = rulesNode;
-        this.projectionRuleId = rulesNode.id;
-        this.projectionNode.config.properties.rules = [this.projectionRuleId];
-        TopologyREST.updateNode(topologyId, versionId, nodeType, nodeData.nodeId, {
-          body: JSON.stringify(this.projectionNode)
-        });
+        if(rulesNode.responseMessage !== undefined){
+          stateObj.showLoading = true;
+          FSReactToastr.error(
+            <CommonNotification flag="error" content={rulesNode.responseMessage}/>, '', toastOpt);
+        } else {
+          this.projectionRulesNode = rulesNode;
+          this.projectionRuleId = rulesNode.id;
+          this.projectionNode.config.properties.rules = [this.projectionRuleId];
+          TopologyREST.updateNode(topologyId, versionId, nodeType, nodeData.nodeId, {
+            body: JSON.stringify(this.projectionNode)
+          });
+        }
       });
     }
     this.setState(stateObj);
