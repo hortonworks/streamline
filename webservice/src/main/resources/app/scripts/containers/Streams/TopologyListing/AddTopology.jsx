2fbc7864b1eb8489729551ea83b81b89d56feaa4
Sanket Shah <er.sanketshah@gmail.com>
2017-10-31 16:00:34 +0530

    ISSUE-994: Topology Name should not have spaces in it.

--- webservice/src/main/resources/app/scripts/containers/Streams/TopologyListing/AddTopology.jsx
+++ webservice/src/main/resources/app/scripts/containers/Streams/TopologyListing/AddTopology.jsx
@@ -86,6 +86,9 @@ class AddTopology extends Component {
     } else if (!/[A-Za-z0-9]/g.test(topologyName)) {
       validDataFlag = false;
       this.setState({validInput: false});
+    } else if(Utils.checkWhiteSpace(topologyName)){
+      validDataFlag = false;
+      this.setState({validInput: false});
     } else {
       validDataFlag = true;
       this.setState({validInput: true});
