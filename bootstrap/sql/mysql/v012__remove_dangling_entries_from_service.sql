49de90123175bb544b1873b251dff9bc27794869
Sarvanan Raju <sraju@hortonworks.com>
2018-01-29 14:31:04 +0530

    ISSUE 1194 : While upgrading remove dangling entries in service when a service pool was removed in 3.0.2

---
+++ bootstrap/sql/mysql/v012__remove_dangling_entries_from_service.sql
@@ -0,0 +1,17 @@
+-- Copyright 2018 Hortonworks.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--    http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+DELETE FROM `service` WHERE `clusterId` NOT IN (SELECT `id` FROM `cluster`);
+DELETE FROM `service_configuration` WHERE `serviceId` NOT IN (SELECT `id` FROM `service`);
+DELETE FROM `component` WHERE `serviceId` NOT IN (SELECT `id` FROM `service`);
